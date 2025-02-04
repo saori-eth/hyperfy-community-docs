7a43b3121e2a16c674035b7888d23f0eaf5034d9
saori-eth <d84da@proton.me>
2025-02-03 21:25:49 -0600

    fix metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,8 +33,8 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Community Documentation`}
+      description="How to do things with Hyperfy">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
