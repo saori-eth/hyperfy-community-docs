80fbf44f2e8f33cd74ffb10ee7b8c4b19d29a288
saori-eth <d84da@proton.me>
2025-02-03 21:36:50 -0600

    dark mode

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -50,6 +50,10 @@ const config: Config = {
   ],
 
   themeConfig: {
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: true,
+    },
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
